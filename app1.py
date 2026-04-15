84e962e7163340e854293716a6d100a698736923
snitcl <tjenko2309@gmail.com>
2026-04-15 11:44:26 +0000

    poprava naloge 1

--- app1.py
+++ app1.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, session
+from tinydb import TinyDB
 
 app = Flask(__name__, template_folder="templates1", static_folder="static1")
-app.secret_key = "supersecretkey"
+app.secret_key = "secret"
 
-# seznam uporabnikov
-users = []
-notes = []
+# Pot do baze
+db = TinyDB('db/notes.json')
+users_table = db.table('users')
+notes_table = db.table('notes')
 
 # LOGIN
 @app.route("/login", methods=["GET", "POST"])
@@ -13,17 +15,23 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+
+        # Preverjanje uporabnika
+        user = users_table.search(lambda u: u['username'] == username)
         
-        for user in users:
-            if user["username"] == username and user["password"] == password:
-                session["username"] = username
-                return redirect("/")
+        if user and user[0]['password'] == password:
+            session["username"] = username
+            return redirect("/")
+
     return render_template("login.html")
 
+
+# LOGOUT
 @app.route("/logout")
 def logout():
     session.pop("username", None)
     return redirect("/login")
+
 
 # REGISTER
 @app.route("/register", methods=["GET", "POST"])
@@ -31,32 +39,46 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+
         
-        if username and all(user["username"] != username for user in users):
-            users.append({"username": username, "password": password})
+        user = users_table.search(lambda u: u['username'] == username)
+        
+        if not user:
+            users_table.insert({'username': username, 'password': password})
             return redirect("/login")
+
     return render_template("register.html")
 
-# INDEX 
+
+# INDEX
 @app.route("/", methods=["GET", "POST"])
 def index():
     if "username" not in session:
         return redirect("/login")
-    
+
     if request.method == "POST":
         title = request.form["title"]
         content = request.form["content"]
-        notes.append({"title": title, "content": content})
+
+        
+        notes_table.insert({'title': title, 'content': content})
         return redirect("/")
 
+    notes = notes_table.all()
     return render_template("index.html", notes=notes)
+
 
 # DELETE
 @app.route("/delete/<int:index>")
 def delete(index):
+    notes = notes_table.all()
+
+    
     if 0 <= index < len(notes):
-        notes.pop(index)
+        notes_table.remove(doc_ids=[index + 1])
+
     return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
