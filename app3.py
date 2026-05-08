70d86e240378f7bec1e9a70f4bffcf7af522cdb9
snitcl <tjenko2309@gmail.com>
2026-05-08 08:48:21 +0000

    dokončno

--- app3.py
+++ app3.py
@@ -1,33 +1,48 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, redirect, session
+from tinydb import TinyDB
 
-# Ustvarimo aplikacijo
 app = Flask(__name__, template_folder="templates3", static_folder="static3")
-app.secret_key = "12345" 
+app.secret_key = "secret"
 
-# Glavna stran
-@app.route("/")
+db = TinyDB("db/polls.json")
+polls = db.table("polls")
+
+if len(polls) == 0:
+    polls.insert({
+        "question": "Katera je tvoja najljubša barva?",
+        "options": ["Rdeča", "Modra", "Zelena", "Rumena"],
+        "votes": [0, 0, 0, 0]
+    })
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if "tasks" not in session:
-        session["tasks"] = []  
-    return render_template("index.html", tasks=session["tasks"])
+    poll = polls.all()[0]  
 
-# Dodajanje opravila (AJAX)
-@app.route("/add", methods=["POST"])
-def add_task():
-    task = request.json.get("task")  
-    if "tasks" not in session:
-        session["tasks"] = []
-    session["tasks"].append(task)    
-    return jsonify({"task": task})   
+    
+    if "has_voted" not in session:
+        session["has_voted"] = False
 
-# AJAX
-@app.route("/delete", methods=["POST"])
-def delete_task():
-    task = request.json.get("task")  
-    if "tasks" in session and task in session["tasks"]:
-        session["tasks"].remove(task)  
-    return jsonify({"task": task})    
+    
+    if request.method == "POST":
+        if session["has_voted"] == False:
+            izbrana = int(request.form["option"])  
+            poll["votes"][izbrana] = poll["votes"][izbrana] + 1  
+            polls.update({"votes": poll["votes"]}, doc_ids=[polls.all()[0].doc_id])
+            session["has_voted"] = True
+        return redirect("/")
 
-# Zaženemo aplikacijo
+    
+    return render_template("index.html", poll=poll, has_voted=session["has_voted"])
+
+
+@app.route("/reset")
+def reset():
+    poll = polls.all()[0]
+    poll["votes"] = [0,0,0,0]
+    polls.update({"votes": poll["votes"]}, doc_ids=[polls.all()[0].doc_id])
+    session["has_voted"] = False
+    return redirect("/")
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
